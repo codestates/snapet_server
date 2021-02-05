a037627e5eba3093cb9f17f4d6f8b8db6d0e9b8e
hyeon0416 <69492986+hyeon0416@users.noreply.github.com>
2021-02-05 11:17:55 +0900

    [feat] create server frame

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express = require('express')
 
 const app = express()
+
 app.use('/',(req,res)=>{
-    res.send('server test node server')
+    res.send('dkdkdkdk')
 })
 
-app.use('/login',(req,res)=>{
-    res.send(data)
-})
-app.listen(5000,()=>{
-    console.log('server is running')
-})
+app.listen(5000)
