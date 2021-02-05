4de9df07e6a5167eb0af0bb5336716140e11c263
조슬지 <seulji@joseuljiui-iMac.local>
2021-02-05 17:03:58 +0900

    [feat]mvc and migration

--- README.md
+++
@@ -1,1 +0,0 @@
-# snapet_server
---
+++ models/users.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  users.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    profilepath: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'users',
+  });
+  users.associate = models => {
+    users.hasMany(models.photos, {
+      foreignKey: 'user_id'
+    })
+    users.belongsToMany(models.photos, {
+      through: 'users_like_photos'
+    })
+  }
+  return users;
+};
