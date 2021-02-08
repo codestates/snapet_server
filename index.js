fce812916441de9e603f834ad3f8120f465c1d32
hyeon0416 <69492986+hyeon0416@users.noreply.github.com>
2021-02-08 14:48:51 +0900

    [feat]https

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const https = require('https')
+const fs = require('fs')
 
 const app = express()
 
-app.use('/',(req,res)=>{
-    res.send('dkdkdkdk')
-})
+    https.createServer({
+        key: fs.readFileSync(__dirname + '/key.pem','utf-8'),
+        cert: fs.readFileSync(__dirname + '/cert.pem','utf-8'),
+    },
+    app.use('/',(req,res)=>{
+        res.send('made https server')
+    })
+)
+
 
 app.listen(5000)
