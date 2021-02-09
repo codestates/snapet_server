6cf728cdaff404c9be6224b0674f4ab99bbf6ed5
조슬지 <seulji@joseuljiui-iMac.local>
2021-02-09 22:13:01 +0900

    [feat]adding method pages

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const https = require('https')
+const fs = require('fs')
+const deletePhoto = require('./controller/deletePhoto');
 
 const app = express()
 
-app.use('/',(req,res)=>{
-    res.send('dkdkdkdk')
-})
-
+app.post('/deletePhoto', deletePhoto.post);
+    https.createServer({
+        key: fs.readFileSync(__dirname + '/../../../../Seulji/keys/key.pem','utf-8'),
+        cert: fs.readFileSync(__dirname + '/../../../../Seulji/keys/cert.pem','utf-8'),
+    },
+    app.use('/',(req,res)=>{
+        res.send('made https server')
+    })
+)
 app.listen(5000)
