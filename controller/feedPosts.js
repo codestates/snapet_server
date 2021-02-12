fd130f079da0f42146772cfe3f2f5ec777bd2aff
조슬지 <seulji@joseuljiui-iMac.local>
2021-02-12 10:54:07 +0900

    [feat]some methods

--- controller/feedPosts.js
+++ controller/feedPosts.js
@@ -1,4 +1,33 @@
-const models = require('../models')
-module.exports = {
+const { photos, users, users_like_photos, sequelize } = require('../models');
+console.log('check!');
+module.exports = async (req, res) => {
+  const posts = await photos.findAll({
+    attributes: {
+      exclude: ['userId'],
+      include: [
+        'id',
+        [sequelize.col('users.name'), 'username'],
+        [
+          sequelize.fn(
+            'count',
+            sequelize.col('users.users_like_photos.userId')
+          ),
+          'countLike',
+        ],
+        // [sequelize.col('users.users_like_photos.countLike'), 'countLike'],
+      ],
+    },
+    group: ['id'],
+    // 그룹바이 에러 뜰때 :
+    // mysql > SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+    include: [
+      {
+        model: users,
+        as: 'users',
+        attributes: [],
+      },
+    ],
+  });
 
-}
+  res.status(200).json(posts);
+};
